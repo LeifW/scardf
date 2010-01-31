995578fc2dfdb678cae96657ac8fa5ac5cc0dfe9
hrvojesimic <hrvojesimic@9ecf9b1c-0d83-11de-8db4-35e3754be71a>
2010-01-31 07:35:06 +0000

    moved RdfList-related converters from RdfList.scala added asDouble and asBigDecimal converters NodeConverter methods "option" and "default" changed their interface: they now create converters operating on singleNode rather than some node, and therefore throwing an exception if the bag contains multiple nodes
    git-svn-id: http://scardf.googlecode.com/svn/trunk@66 9ecf9b1c-0d83-11de-8db4-35e3754be71a

--- src/main/scala/net/croz/scardf/NodeBagConverter.scala
+++ src/main/scala/net/croz/scardf/NodeBagConverter.scala
@@ -3,7 +3,7 @@ package net.croz.scardf
 import org.joda.time.{ LocalDate, DateTime }
 
 /**
- * Apply method of a constructor will convert given node bag into some other object,
+ * Apply method converts given node bag into some other object,
  * using the function given in constructor.
  */
 class NodeBagConverter[T]( fn: NodeBag => T ) extends (NodeBag => T) {
@@ -11,30 +11,28 @@ class NodeBagConverter[T]( fn: NodeBag => T ) extends (NodeBag => T) {
 }
 
 /**
- * Converts bag to a single node, typically a literal value.
+ * Converts a single-node bag to an object of type T (typically a literal value),
+ * given a Node => T function.
+ * Apply method throws an RdfTraversalException if the size of the bag is not 1.
  */
 class NodeConverter[T]( fn: Node => T )
 extends NodeBagConverter[T]( bag => fn( bag.singleNode ) ) 
 {
   /**
    * Constructs another converter which will return None when given an empty bag,
-   * and apply this converter on some containing node otherwise.
+   * and apply this converter on the single node otherwise.
    */
   def option = new NodeBagConverter[Option[T]]( bag => {
-    val nopt = bag.nodeOption
-    if ( nopt.isDefined ) Some( apply( nopt.get ) )
-    else None
+    if ( bag.isEmpty ) None
+    else if ( bag.size > 1 ) throw new RdfTraversalException( "Yielded multiple RDF nodes: " + bag )
+    else Some( this( bag.singleNode ) )
   } )
 
   /**
    * Constructs another converter which will return given default value for an empty bag,
-   * and apply this converter on some containing node otherwise.
+   * and apply this converter on the single node otherwise.
    */
-  def default( defaultValue: T ) = new NodeBagConverter[T]( bag => {
-    val nopt = bag.nodeOption
-    if ( nopt.isDefined ) apply( nopt.get )
-    else defaultValue
-  } )
+  def default( defaultValue: T ) = new NodeBagConverter[T]( bag => option( bag ) getOrElse defaultValue )
 
   /**
    * Constructs another bag converter which will return an Iterable through all values 
@@ -55,14 +53,25 @@ object asLit extends NodeConverter[Lit]( _.asLit )
 object asString extends NodeConverter[String]( _.asString )
 object asBoolean extends NodeConverter[Boolean]( _.asBoolean )
 object asInt extends NodeConverter[Int]( _.asInt )
+object asDouble extends NodeConverter[Double]( _.asDouble )
+object asBigDecimal extends NodeConverter[BigDecimal]( _.asBigDecimal )
 object asLocalDate extends NodeConverter[LocalDate]( _.asLocalDate )
 object asDateTime extends NodeConverter[DateTime]( _.asDateTime )
 
+/** Converts single node to a list. */
+object asRdfList extends NodeConverter[RdfList] ( _.asRdfList )
+
+/** Converts single node to a new bag containing its list elements. */
+object asRdfListBag extends NodeConverter[NodeBag] ( _.asRdfList.toNodeBag )
+
+/**
+ * Converts a node bag to another node bag by filtering its nodes.
+ */
 class NodeBagFilter( ffn: Node => Boolean )
 extends NodeBagConverter[NodeBag]( bag => new NodeBag( bag.list filter ffn ) )
 
 /**
- * Factory object for bag filters
+ * Factory object for bag filters.
  * @see NodeBagFilter
  */
 object where {
